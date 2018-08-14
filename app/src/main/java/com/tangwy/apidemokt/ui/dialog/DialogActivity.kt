9c40258e8964b1ecd5dff0d3e0c11810f307c29a
Troy <550795911@qq.com>
2018-08-14 22:27:49 +0800

    update code style

--- app/src/main/java/com/tangwy/apidemokt/ui/dialog/DialogActivity.kt
+++ app/src/main/java/com/tangwy/apidemokt/ui/dialog/DialogActivity.kt
@@ -2,6 +2,7 @@ package com.tangwy.apidemokt.ui.dialog
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Toast
 import com.tangwy.apidemokt.framework.dialog.DialogAPI
 import com.tangwy.apidemokt.R
@@ -10,7 +11,19 @@ import com.tangwy.apidemokt.framework.dialog.ListDialog
 import kotlinx.android.synthetic.main.content_dialog.*
 
 
-class DialogActivity: AppCompatActivity(), CallbackDialog.Callback {
+class DialogActivity : AppCompatActivity(), CallbackDialog.Callback, View.OnClickListener {
+
+    override fun onClick(v: View?) {
+        when (v) {
+            common -> DialogAPI.createCommon().show(supportFragmentManager, "Common")
+            single -> DialogAPI.createList(ListDialog.Type.Single).show(supportFragmentManager, "Single")
+            single_permanent -> DialogAPI.createList(ListDialog.Type.SinglePermanent).show(supportFragmentManager, "Single Permanent")
+            multi_permanent -> DialogAPI.createList(ListDialog.Type.MultiPermanent).show(supportFragmentManager, "Multi Permanent")
+            custom -> DialogAPI.createCustom(R.layout.dialog_signin).show(supportFragmentManager, "Custom")
+            callback -> DialogAPI.createCallback().show(supportFragmentManager, "Callback")
+            embedded -> DialogAPI.createEmbedded(R.layout.dialog_signin).show(supportFragmentManager, "Embedded")
+        }
+    }
 
     override fun onPositiveClick() {
         Toast.makeText(this, "onPositiveClick Callback from CallbackDialog", Toast.LENGTH_SHORT).show()
@@ -24,60 +37,12 @@ class DialogActivity: AppCompatActivity(), CallbackDialog.Callback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialog)
 
-        common.setOnClickListener {
-            showCommon()
-        }
-
-        single.setOnClickListener {
-            showSingleList()
-        }
-
-        single_permanent.setOnClickListener {
-            showSinglePermanentList()
-        }
-
-        multi_permanent.setOnClickListener {
-            showMultiPermanentList()
-        }
-
-        custom.setOnClickListener {
-            showCustom()
-        }
-
-        callback.setOnClickListener {
-            showCallback()
-        }
-
-        embedded.setOnClickListener {
-            showEmbedded()
-        }
-    }
-
-    private fun showCommon() {
-        DialogAPI.createCommon().show(supportFragmentManager, "Common")
-    }
-
-    private fun showSingleList() {
-        DialogAPI.createList(ListDialog.Type.Single).show(supportFragmentManager, "Single")
-    }
-
-    private fun showSinglePermanentList() {
-        DialogAPI.createList(ListDialog.Type.SinglePermanent).show(supportFragmentManager, "Single Permanent")
-    }
-
-    private fun showMultiPermanentList() {
-        DialogAPI.createList(ListDialog.Type.MultiPermanent).show(supportFragmentManager, "Multi Permanent")
-    }
-
-    private fun showCustom() {
-        DialogAPI.createCustom(R.layout.dialog_signin).show(supportFragmentManager, "Custom")
-    }
-
-    private fun showCallback() {
-        DialogAPI.createCallback().show(supportFragmentManager, "Callback")
-    }
-
-    private fun showEmbedded() {
-        DialogAPI.createEmbedded(R.layout.dialog_signin).show(supportFragmentManager, "Embedded")
+        common.setOnClickListener(this)
+        single.setOnClickListener(this)
+        single_permanent.setOnClickListener(this)
+        multi_permanent.setOnClickListener(this)
+        custom.setOnClickListener(this)
+        callback.setOnClickListener(this)
+        embedded.setOnClickListener(this)
     }
 }
--- app/src/main/java/com/tangwy/apidemokt/ui/notification/NotificationActivity.kt
+++ app/src/main/java/com/tangwy/apidemokt/ui/notification/NotificationActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.tangwy.apidemokt.R
 import com.tangwy.apidemokt.framework.notification.NotificationService
 import com.tangwy.apidemokt.framework.notification.NotificationAPI
@@ -11,81 +12,43 @@ import kotlinx.android.synthetic.main.activity_notification.*
 import kotlinx.android.synthetic.main.content_notification.*
 import java.util.*
 
-class NotificationActivity: AppCompatActivity() {
+class NotificationActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(v: View?) {
+        when (v) {
+            common -> NotificationAPI.notify(this, "Common", "Content")
+            inbox_style -> NotificationAPI.notifyWithStyle(this, "InboxStyle", "Content")
+            update -> {
+                val id = Random().nextInt(1000)
+                var num = 1
+                NotificationAPI.notifyUpdate(this, "Original", num.toString(), id)
+                Handler().postDelayed({
+                    num++
+                    NotificationAPI.notifyUpdate(this, "Update", num.toString(), id)
+                }, 5000)
+            }
+            special -> NotificationAPI.notifySpecial(this, "Special", "Content")
+            progress -> NotificationAPI.notifyProgress(this, "Progress", "Content")
+            activity_indicator -> NotificationAPI.notifyActivityIndicator(this, "Progress", "Content")
+            custom_view -> NotificationAPI.notifyCustom(this, packageName, R.layout.notification_custom)
+            fab -> {
+                val serviceIntent = Intent(applicationContext, NotificationService::class.java)
+                startService(serviceIntent)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
 
-        fab.setOnClickListener {
-            startNotificationService()
-        }
-
-        common.setOnClickListener {
-            notifyCommon()
-        }
-
-        inbox_style.setOnClickListener {
-            notifyInboxStyle()
-        }
-
-        update.setOnClickListener {
-            notifyUpdate()
-        }
-
-        special.setOnClickListener {
-            notifySpecial()
-        }
-
-        progress.setOnClickListener {
-            notifyProgress()
-        }
-
-        activity_indicator.setOnClickListener {
-            notifyActivityIndicator()
-        }
-
-        custom_view.setOnClickListener {
-            notifyCustom()
-        }
-    }
-
-    private fun startNotificationService() {
-        val serviceIntent = Intent(applicationContext, NotificationService::class.java)
-        startService(serviceIntent)
-    }
-
-    private fun notifyCommon() {
-        NotificationAPI.notify(this, "Common", "Content")
-    }
-
-    private fun notifyInboxStyle() {
-        NotificationAPI.notifyWithStyle(this, "InboxStyle", "Content")
-    }
-
-    private fun notifySpecial() {
-        NotificationAPI.notifySpecial(this, "Special", "Content")
-    }
-
-    private fun notifyProgress() {
-        NotificationAPI.notifyProgress(this, "Progress", "Content")
-    }
-
-    private fun notifyActivityIndicator() {
-        NotificationAPI.notifyActivityIndicator(this, "Progress", "Content")
-    }
-
-    private fun notifyCustom() {
-        NotificationAPI.notifyCustom(this, packageName, R.layout.notification_custom)
-    }
-
-    private fun notifyUpdate() {
-        val id = Random().nextInt(1000)
-        var num = 1
-        NotificationAPI.notifyUpdate(this, "Original", num.toString(), id)
-        Handler().postDelayed({
-            num++
-            NotificationAPI.notifyUpdate(this, "Update", num.toString(), id)
-        }, 5000)
+        fab.setOnClickListener(this)
+        common.setOnClickListener(this)
+        inbox_style.setOnClickListener(this)
+        update.setOnClickListener(this)
+        special.setOnClickListener(this)
+        progress.setOnClickListener(this)
+        activity_indicator.setOnClickListener(this)
+        custom_view.setOnClickListener(this)
     }
 }
